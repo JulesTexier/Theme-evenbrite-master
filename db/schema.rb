d179f3ad29fe6d55299ed6dff2c485a37665d18f
Texier <julestexier@live.fr>
2020-11-03 15:38:18 +0100

    bdd

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 2020_11_03_103912) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
